5d1584b9f46f0af54549d19dc272a22613ddfbdf
ilya.kuznetsov <ilya.kuznetsov@tabby.ai>
2025-05-22 14:07:26 +0800

    🛠 TECH: improve android build script

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -5,4 +5,27 @@ echo "Building Android 🛠️"
 cd example
 
 fvm flutter build apk --release
-echo "✓ Built example/build/app/outputs/flutter-apk/app-release.apk"
+
+cd ..
+
+# Prepare version
+fvm dart run scripts/prepare_version.dart
+
+eval "$(
+  cat ./scripts/.env_version | awk '!/^\s*#/' | awk '!/^\s*$/' | while IFS='' read -r line; do
+    key=$(echo "$line" | cut -d '=' -f 1)
+    value=$(echo "$line" | cut -d '=' -f 2-)
+    echo "export $key=\"$value\""
+  done
+)"
+
+echo "📦 got version $TABBY_APP_VERSION+$TABBY_APP_BUILD_NUMBER"
+
+version_name="$TABBY_APP_VERSION($TABBY_APP_BUILD_NUMBER)"
+
+echo "👉 Currently in dir: $(pwd)"
+
+cp example/build/app/outputs/flutter-apk/app-release.apk example/build/app/outputs/flutter-apk/tabby-flutter-sdk-demo-app-$version_name.apk
+
+echo "✓ Built example/build/app/outputs/flutter-apk/tabby-flutter-sdk-demo-app-$version_name.apk"
+ls example/build/app/outputs/flutter-apk/tabby-flutter-sdk-demo-app-*.apk
